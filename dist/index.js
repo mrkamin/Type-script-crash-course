d43cd948e4c81ab84b2a326d3b09e262c550dfa2
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-03-16 19:06:45 +0500

    working with object

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
 "use strict";
-function calculateTax(incom, taxYear = 2022) {
-    if (taxYear < 2022)
-        return incom * 1.2;
-    return incom * 1.3;
-}
-console.log("incom");
-calculateTax(10000);
+let employee = {
+    id: 1,
+    name: 'Rafi',
+    retire: (date) => {
+        console.log(date);
+    }
+};
 //# sourceMappingURL=index.js.map
