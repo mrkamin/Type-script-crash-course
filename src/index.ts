95eb72fb63e1c73df446cde9fb7c114cab955b73
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-03-17 05:09:15 +0500

    TS Intersection type example

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-function kgToLbs(weight: number | string): number {
-    if(typeof weight === 'number')
-    return weight * 2.2;
-else
-return parseInt(weight) * 2.2;
+type Draggable = {
+    drag: () => void
+};
+
+type Resizable = {
+    resize: () => void
 }
 
-kgToLbs(10);
-kgToLbs('10kg');
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {}
+}
