3fcf6356733316ade0a5df3c0d1ad91f96d72e04
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-03-18 05:42:38 +0500

    Make the birthday property optional and use optional property access operateor

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 type Customer = {
-  birthday: Date
+  birthday?: Date
 };
 
 function getCustomer(id: number): Customer | null | undefined {
   return id === 0 ? null : {birthday: new Date() };
 }
 
-let customer = getCustomer(0);
+let customer = getCustomer(1);
 //if (customer !== null && customer !== undefined)
 // Optional property access operator
-console.log(customer?.birthday)
+console.log(customer?.birthday?.getFullYear())
