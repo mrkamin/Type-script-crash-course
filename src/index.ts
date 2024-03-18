bd2a077912ad5df60b58f5bcc0f6ea172986b071
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-03-18 05:36:11 +0500

    optional changing example

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ type Customer = {
   birthday: Date
 };
 
-function getCustomer(id: number): Customer | null {
+function getCustomer(id: number): Customer | null | undefined {
   return id === 0 ? null : {birthday: new Date() };
 }
 
 let customer = getCustomer(0);
-if (customer !== null)
+if (customer !== null && customer !== undefined)
 console.log(customer.birthday)
