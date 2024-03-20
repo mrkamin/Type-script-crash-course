054b1629dca7f4e0fced323a75a9b50b61a8e187
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-03-20 05:19:47 +0500

    TS object example

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,4 @@
 "use strict";
-var _a;
-function getCustomer(id) {
-    return id === 0 ? null : { birthday: new Date() };
-}
-let customer = getCustomer(1);
-console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+let user = { name: "Rafi", age: "33" };
+console.log(user.name);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-console.log("Rafi")
+let user = {name: "Rafi", age: "33"};
+
+console.log(user.name)
