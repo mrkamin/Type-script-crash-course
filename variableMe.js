c111754dc163286830d5bacef98c0222e77abd36
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-03-21 19:46:55 +0500

    TS number type example

--- variableMe.js
+++ variableMe.js
@@ -3,3 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello World";
 greetings.toLowerCase();
 console.log(greetings);
+var userId = 334455.3;
--- variableMe.ts
+++ variableMe.ts
@@ -1,7 +1,12 @@
+//string
 let greetings: string = "Hello World";
 
-greetings.toLowerCase()
+greetings.toLowerCase();
 
 console.log(greetings);
 
-export {}
+// number
+
+let userId: number = 334455.3;
+
+export {};
