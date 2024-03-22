8fd7dec537b110949ea2b84df3150813b35ec9f6
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-03-22 13:39:59 +0500

    Using of string, number, and boolean type in one fun together exm

--- variableMe.ts
+++ variableMe.ts
@@ -8,5 +8,10 @@ function getUpper(val: string) {
     return val.toUpperCase()
 }
 
+function signUpUser(name: string, email: string, isPaid: boolean) {
+
+}
+
 addTwo(5);
 getUpper("Rafi")
+signUpUser("Rafi","rafi.amin@justcold.co.uk", false);
