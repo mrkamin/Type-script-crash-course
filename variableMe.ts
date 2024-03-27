2bc3a73967b58fe5b5fb41a5dbbfbc76d68ea246
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-03-27 18:22:21 +0500

    use of readonly example in TS

--- variableMe.ts
+++ variableMe.ts
@@ -1,11 +1,28 @@
+// type User = {
+//     name: string;
+//     email: string;
+//     isActive: boolean
+// }
+
+// function createUser(user: User): User{
+//     return { name: "", email: "", isActive: true}
+// }
+
+// createUser({name: "", email: "", isActive: true})
+
 type User = {
-    name: string;
-    email: string;
-    isActive: boolean
-}
+  readonly _id: string;
+  name: string;
+  email: string;
+  isActive: boolean;
+};
 
-function createUser(user: User): User{
-    return { name: "", email: "", isActive: true}
-}
+let myUser: User = {
+  _id: "12345",
+  name: "Rafi",
+  email: "rafi@rafi.com",
+  isActive: false,
+};
 
-createUser({name: "", email: "", isActive: true})
+myUser.email = "khan@khan.com";
+myUser._id = "4455";
