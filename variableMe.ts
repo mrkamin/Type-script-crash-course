9874ea2411b7fdf89e507032797abf5652938a04
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-03-28 04:54:47 +0500

    usein & example

--- variableMe.ts
+++ variableMe.ts
@@ -10,20 +10,32 @@
 
 // createUser({name: "", email: "", isActive: true})
 
-type User = {
-  readonly _id: string;
-  name: string;
-  email: string;
-  isActive: boolean;
-  cardDetails?: number
-};
+// type User = {
+//   readonly _id: string;
+//   name: string;
+//   email: string;
+//   isActive: boolean;
+//   cardDetails?: number
+// };
 
-let myUser: User = {
-  _id: "12345",
-  name: "Rafi",
-  email: "rafi@rafi.com",
-  isActive: false,
-};
+// let myUser: User = {
+//   _id: "12345",
+//   name: "Rafi",
+//   email: "rafi@rafi.com",
+//   isActive: false,
+// };
 
-myUser.email = "khan@khan.com";
-myUser._id = "4455";
+// myUser.email = "khan@khan.com";
+// myUser._id = "4455";
+
+type cardNumber = {
+  cardnumber: string
+}
+
+type cardDate = {
+  cardDate: string
+}
+
+type cardDetails = cardNumber & cardDate & {
+   cvv: number
+}
