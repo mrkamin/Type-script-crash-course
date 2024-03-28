5ab792a85dcdc013395107222351892e89693c49
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-03-28 06:25:45 +0500

    Use of type alias object with Array in to TS example

--- variableMe.ts
+++ variableMe.ts
@@ -3,5 +3,14 @@ const heroPower: number[] = []
 
 const heroPowerTwo: Array<number> = []
 
+type User = {
+  name: string 
+  isActive: boolean
+}
+
+const allUsers: User[] = []
+
 superHeros.push("siderman")
 heroPower.push(2)
+
+allUsers.push({name: "", isActive: true})
