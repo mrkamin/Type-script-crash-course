43e12ca331a884d5543b488707eb4dace822db74
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-03-28 06:34:26 +0500

    Use of Union Type in to TS example

--- variableMe.ts
+++ variableMe.ts
@@ -1,21 +1,4 @@
-const superHeros: string[] = []
-const heroPower: number[] = []
+let score: number | string = 33
 
-const heroPowerTwo: Array<number> = []
-
-type User = {
-  name: string 
-  isActive: boolean
-}
-
-const allUsers: User[] = []
-
-const arrayOfArray: number[][] =[
-  [255, 255, 255],
-  []
-]
-
-superHeros.push("siderman")
-heroPower.push(2)
-
-allUsers.push({name: "", isActive: true})
+score = 44
+score = "55"
