6c13e80e58836b6f66623f9efcf2fc0af03ea321
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-03-28 06:38:27 +0500

    Use of | type with type aliase in to the TS example

--- variableMe.ts
+++ variableMe.ts
@@ -2,3 +2,17 @@ let score: number | string = 33
 
 score = 44
 score = "55"
+
+type User = {
+  name: string
+  id: number
+}
+
+type Admin  ={
+  username: string
+  id: number
+}
+
+let rafi: User | Admin = {
+  name: "Rafi", id: 222
+}
